413ea52d44bba718a138a3e350d23b11207ed99c
jms <janne.markus.salonen@gmail.com>
2021-02-28 22:25:00 +0200

    joop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        Header
+      </div>
+    )
+  }
+}
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        Home
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      eka: 0,
+      toka: 0
+    }
+
+    this.handleEka = this.handleEka.bind(this)
+  }
+
+  handleEka() {
+    let temp1 = this.state.eka
+    ++temp1 
+    this.setState({ eka: temp1 })
+  }
+
+  handleToka = () => {
+    let temp2 = this.state.toka
+    ++temp2
+    this.setState({ toka: temp2 })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Home />
+        <div>
+          <button onClick={() => this.handleEka()}>Eka</button>
+        </div>
+        <div>
+          <button onClick={this.handleToka}>toka</button>
+        </div>
+        {JSON.stringify(this.state)}
+      </div>
+    )
+  }
 }
 
 export default App;
